c03973361c076a52ca10d0855d338e48c3527fa4
Samilis Brito <samilisbritto@gmail.com>
2025-03-25 08:56:53 -0300

    first commit

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+  <main class="conteudo-principal">
+    <section>
+      <span class="subtitulo-lg sua-lista-texto">
+        Sua lista:
+      </span>
+      <ul class="ingredientes-sua-lista">
+        <li class="ingrediente">
+          Alho
+        </li>
+        <li class="ingrediente">
+          Feijão
+        </li>
+        <li class="ingrediente">
+          Macarrão
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
