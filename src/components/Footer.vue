26471cd2e4803cd264bb6a447e9f03d574be7032
Samilis Brito <samilisbritto@gmail.com>
2025-03-30 22:35:05 -0300

    feat(componente): rodapé

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+  <footer>
+    <p class="paragrafo">Desenvolvido por Alura | 2023 - Projeto fictício sem fins comerciais.</p>
+  </footer>
+</template>
+
+<style scoped>
+.paragrafo {
+  text-align: center;
+  padding: 1.5rem;
+  background: var(--verde-escuro, #263A29);
+}
+</style>
