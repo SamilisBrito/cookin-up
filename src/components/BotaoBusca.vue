7a0a9d23a0cfd38b68e90f22423186b2c9b73d93
Samilis Brito <samilisbritto@gmail.com>
2025-03-30 22:34:34 -0300

    feat(componente): botão de busca

---
+++ src/components/BotaoBusca.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default {
+}
+</script>
+
+<template>
+  <button class="botao-busca">
+    Buscar receitas!
+  </button>
+</template>
+
+<style scoped>
+.botao-busca {
+  background-color: var(--coral);
+  color: var(--branco);
+  font-size: 1.25rem;
+  font-weight: 700;
+  padding: 0.75rem 4.6875rem;
+  border-radius: 3.125rem;
+}
+</style>
